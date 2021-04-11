9b7f9aa25ba41be230bf125eab9db0dce1c8c91b
Julian Phillips <julian@julianlp.com>
2021-04-11 01:18:48 -0600

    Formatting and added ESLint rule ignore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />; // eslint-disable-line react/jsx-props-no-spreading
 }
 
 export default MyApp;
