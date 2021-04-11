ab851f82131eecacdb3f2598530cab6ee5d48f48
Julian Phillips <julian@julianlp.com>
2021-04-11 01:16:41 -0600

    Linting

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>JulianLP</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>JulianLP</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-            <div>
-                <h1>hello world</h1>
-            </div>
-        </div>
-    );
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </div>
+  );
 }
