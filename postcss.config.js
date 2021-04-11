055083e6ae653e75618c41bc7a320b05f3441acd
Julian Phillips <julian@julianlp.com>
2021-04-11 01:15:27 -0600

    Added StyleLint to support Tailwind errors; updated ESLint, Prettier, and stylelint configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+  plugins: [
+    require('postcss-import'), // eslint-disable-line global-require
+    require('tailwindcss'), // eslint-disable-line global-require
+    require('postcss-preset-env')({ stage: 1 }), // eslint-disable-line @typescript-eslint/no-var-requires, global-require
+  ],
 };
