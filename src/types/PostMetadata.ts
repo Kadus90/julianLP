fe04769a179addbe420dab81ab213a6b152156c1
Julian Phillips <julian@julianlp.com>
2021-04-13 21:02:55 -0600

    Initiated PostMetadata

---
+++ src/types/PostMetadata.ts
@@ -0,0 +1,11 @@
+export type PostMetadata = {
+  id: string;
+  titile: string;
+  date: string;
+  coverUrl: string;
+  content: string;
+  tags: string[];
+  description: string;
+  theme: string;
+  series: string;
+};
