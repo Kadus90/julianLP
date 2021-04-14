ab2d018ca77074a93544c2f5fa8b89e0552f5d0b
Julian Phillips <julian@julianlp.com>
2021-04-13 20:45:36 -0600

    Added css modules support

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatters: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '\\.scss|sass|css)$': 'identity-obj-proxy',
+  },
+  moduleDirectories: ['node_modules', 'src'],
 };
