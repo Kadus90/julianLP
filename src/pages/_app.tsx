ca7e46e66b3befde1cc7020c6d7c5a26bb4cb2ca
Julian Phillips <julian@julianlp.com>
2021-04-13 20:57:34 -0600

    Updated app to import Layout component, fontawesome, and ThemeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import Layout from '../components/Layout/Layout';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />; // eslint-disable-line react/jsx-props-no-spreading
+  library.add(fab);
+  return (
+    <ThemeProvider attribute="class" enableSystem>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
