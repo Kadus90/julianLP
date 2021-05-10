0e988f32aa8b19c33cf2afe953778d9be2bb8b57
Julian Phillips <julian@julianlp.com>
2021-05-10 15:13:41 -0400

    Extended tailwind features for hover effects

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    scale: {
+      0: '0',
+      25: '.25',
+      50: '.5',
+      75: '.75',
+      90: '.9',
+      95: '.95',
+      100: '1',
+      105: '1.05',
+      110: '1.1',
+      125: '1.25',
+      150: '1.5',
+      200: '2',
+    },
     extend: {
       colors: {
         'cool-gray': colors.coolGray,
@@ -11,7 +25,14 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+      divideColor: ['group-hover'],
+      hueRotate: ['hover', 'focus'],
+      padding: ['hover'],
+      scale: ['active', 'group-hover'],
+      transform: ['hover', 'focus'],
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 };
