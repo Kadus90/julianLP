a094767b2ebac4cc691c4122f0821be8a1a72d12
Julian Phillips <julian@julianlp.com>
2021-05-11 11:25:57 -0400

    hid logo, pruned imports

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useContext } from 'react';
+import { FC, useState } from 'react';
 import Config from '../../config';
 import NavbarLink from './NavbarLink';
 import HamburgerMenu from './HamburgerMenu';
@@ -6,6 +6,7 @@ import SiteLogo from '../Logo/SiteLogo';
 import SocialLinks from '../Links/SocialLinks';
 import { useTheme } from 'next-themes';
 import ThemeButton from '../Buttons/ThemeButton';
+import Link from 'next/link';
 
 type NavbarProps = {
   showLinks?: boolean;
@@ -16,60 +17,68 @@ const Navbar: FC<NavbarProps> = (props: NavbarProps) => {
   const { theme } = useTheme();
 
   return (
-    <nav className="bg-cool-gray-300 dark:bg-cool-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {showLinks && (
-              <HamburgerMenu
-                handleOnTap={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}
-              />
-            )}
-          </div>
-          <div className="flex-1 flex items-center justify-center sm:justify-start">
-            <SiteLogo></SiteLogo>
-            {/* Nav buttons for desktop */}
-            <div className="hidden sm:block sm:ml-6 flex-1">
+    <>
+      <nav className="bg-cool-gray-300 dark:bg-cool-gray-800">
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-16">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               {showLinks && (
-                <div className="flex space-x-4">
-                  {Config.navbarLinks.map((link) => (
-                    <NavbarLink
-                      key={link.href}
-                      type="DESKTOP"
-                      text={link.text}
-                      href={link.href}
-                    ></NavbarLink>
-                  ))}
-                </div>
+                <HamburgerMenu
+                  handleOnTap={() => setMobileMenuIsOpen(!mobileMenuIsOpen)}
+                />
               )}
             </div>
-
-            <ThemeButton />
-            <div className="hidden md:block">
-              <SocialLinks
-                color={theme === 'light' ? 'dark' : 'light'}
-              ></SocialLinks>
+            <div className="flex-1 flex items-center justify-center sm:justify-start">
+              {/* <SiteLogo /> */}
+              <Link href="/">
+                <a>
+                  <h1 className="text-4xl tracking-tight font-extrabold">
+                    Julian<span className="text-lightBlue-700">LP</span>
+                  </h1>
+                </a>
+              </Link>
+              {/* Nav buttons for desktop */}
+              <div className="hidden sm:block sm:ml-6 flex-1">
+                {showLinks && (
+                  <div className="flex space-x-4">
+                    {Config.navbarLinks.map((link) => (
+                      <NavbarLink
+                        key={link.href}
+                        type="DESKTOP"
+                        text={link.text}
+                        href={link.href}
+                      ></NavbarLink>
+                    ))}
+                  </div>
+                )}
+              </div>
+              <ThemeButton />
+              <div className="hidden md:block">
+                <SocialLinks
+                  color={theme === 'light' ? 'dark' : 'light'}
+                ></SocialLinks>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Nav buttons for mobile */}
-      {showLinks && (
-        <div className={mobileMenuIsOpen ? 'block' : 'hidden'}>
-          <div className="px-2 pt-2 pb-3 space-y-1">
-            {Config.navbarLinks.map((link) => (
-              <NavbarLink
-                key={link.href}
-                type="MOBILE"
-                text={link.text}
-                href={link.href}
-              ></NavbarLink>
-            ))}
+        {/* Nav buttons for mobile */}
+        {showLinks && (
+          <div className={mobileMenuIsOpen ? 'block' : 'hidden'}>
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {Config.navbarLinks.map((link) => (
+                <NavbarLink
+                  key={link.href}
+                  type="MOBILE"
+                  text={link.text}
+                  href={link.href}
+                ></NavbarLink>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </nav>
+        )}
+      </nav>
+    </>
   );
 };
 
