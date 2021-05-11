11e129a27ff671f0827bc4ebe63ab2d700093e8f
Julian Phillips <julian@julianlp.com>
2021-05-11 12:26:51 -0400

    updated to construct tag spans and then inject them into the component

--- src/components/Lists/TagList.tsx
+++ src/components/Lists/TagList.tsx
@@ -5,15 +5,20 @@ type TagListProps = {
 };
 
 const TagList: FC<TagListProps> = ({ tags }: TagListProps) => {
-  return (
-    <p className="flex">
-      {tags.map((tag) => {
-        <span className="bg-cool-gray-700 text-white mr-2 rounded-lg py-1 px-2 text-sm">
-          {tag}
-        </span>;
-      })}
-    </p>
-  );
+  console.log(tags);
+
+  const tagArray = tags.map((tag, i) => {
+    return (
+      <span
+        key={i}
+        className="bg-cool-gray-700 text-white mr-2 rounded-lg py-1 px-2 text-sm"
+      >
+        {tag}
+      </span>
+    );
+  });
+
+  return <p className="flex">{tagArray}</p>;
 };
 
 export default TagList;
