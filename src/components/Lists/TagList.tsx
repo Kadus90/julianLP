b80ba5fa209e846c9b18adae6ada29a56bebdc65
Julian Phillips <julian@julianlp.com>
2021-05-11 12:42:02 -0400

    updated to take theme color as bg color

--- src/components/Lists/TagList.tsx
+++ src/components/Lists/TagList.tsx
@@ -1,18 +1,21 @@
 import { FC } from 'react';
+import { getBackgroundClass } from '../../utils/theme';
+import { useHover } from '../../hooks/useHover';
 
 type TagListProps = {
   tags: string[];
+  theme: string;
 };
 
-const TagList: FC<TagListProps> = ({ tags }: TagListProps) => {
+const TagList: FC<TagListProps> = ({ tags, theme }: TagListProps) => {
   console.log(tags);
 
   const tagArray = tags.map((tag, i) => {
+    const classes = `text-white mr-2 rounded-lg py-1 px-2 text-sm ${getBackgroundClass(
+      theme,
+    )}`;
     return (
-      <span
-        key={i}
-        className="bg-cool-gray-700 text-white mr-2 rounded-lg py-1 px-2 text-sm"
-      >
+      <span key={i} className={useHover(classes, 'hover:scale-110')}>
         {tag}
       </span>
     );
