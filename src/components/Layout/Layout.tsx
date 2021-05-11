569ca43770987ca3388808ff0cc2b09582a31108
Julian Phillips <julian@julianlp.com>
2021-05-11 11:23:40 -0400

    Added transition time

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,8 +2,8 @@ import Navbar from '../Navbar/Navbar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-gray-200 dark:bg-cool-gray-900 text-black dark:text-white min-h-screen">
-      <Navbar></Navbar>
+    <div className="bg-gray-200 dark:bg-cool-gray-900 text-black dark:text-white min-h-screen transition duration-700 ease-in-out">
+      <Navbar />
       {children}
     </div>
   );
